b8a60bf5d39996f6cdb9d60110a4d067fcfcbf97
Nguyễn Quang Dũng <dungchelsea1806@gmail.com>
2024-05-03 13:28:41 +0700

    Add Nodejs and MongoDB for my Web Project

---
+++ src/index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+
+const dotenv = require('dotenv');
+const { default: mongoose } = require("mongoose");
+const bodyParser = require('body-parser')
+
+const routes = require('./routes')
+
+dotenv.config()
+
+const app = express()
+const port = process.env.PORT || 3001
+app.use(bodyParser.json())
+routes(app);
+
+
+mongoose.connect(`${process.env.MONGO_DB}`).then(() => 
+        {
+            console.log('Connect Db success')
+        }
+) .catch((err)=> {
+    console.log(err)
+})
+
+
+
+
+app.listen(port,()=>
+    {
+        console.log('Server is running in port: ',+ port)
+    }
+)
